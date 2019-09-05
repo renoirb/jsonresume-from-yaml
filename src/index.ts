8889ae035df304f8c0bc4db6b64a9b5acbd6503c
Renoir Boulanger <hello@renoirboulanger.com>
2019-09-04 21:49:23 -0400

    release

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ export * from './schema-manager';
 export * from './schema';
 export * from './type-guards';
 export * from './types';
+
+import { default as jsonify } from './jsonify';
+
+export { jsonify };
